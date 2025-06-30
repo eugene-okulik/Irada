3cf5f8c99e89887b4296239c2a020b18006e04cb
Irada <irodasab@gmail.com>
2025-06-30 13:32:37 -0700

    Homework_5 done

---
+++ homework/irada_sab/Homework_5/hw5_3.py
@@ -0,0 +1,11 @@
+students = ['Ivanov', 'Petrov', 'Sidorov']
+subjects = ['math', 'biology', 'geography']
+
+#Распечатайте текст, который будет использовать данные из этих списков. Текст в итоге должен выглядеть так:
+#Students Ivanov, Petrov, Sidorov study these subjects: math, biology, geography
+
+a = ', '.join(students)
+b = ', '.join(subjects)
+for_sentence = 'Students {0}, study these subjects: {1}'
+
+print(for_sentence.format(a, b))
