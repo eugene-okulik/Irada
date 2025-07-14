1a9d7af50f12984dcf1c8caba2613f1d982e6b1a
Irada <irodasab@gmail.com>
2025-07-14 08:56:05 -0700

    Homework_6 fixed with adding the missed file

--- homework/irada_sab/Homework_6/hw6_2.py
+++ homework/irada_sab/Homework_6/hw6_2.py
@@ -11,7 +11,7 @@ for number in my_range:
         print("FuzzBuzz")
     elif number % 3 == 0:
         print("Fuzz")
-    elif number % 5 ==0:
+    elif number % 5 == 0:
         print("Buzz")
     else:
         print(number)
